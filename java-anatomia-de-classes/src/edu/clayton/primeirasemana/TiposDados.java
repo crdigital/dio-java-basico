c29be773609fee97b89f349ffa61fb22b5844065
crdigital <clayton.metal2@hotmail.com>
2023-10-20 11:30:45 -0300

    Tipos Variaveis

---
+++ java-anatomia-de-classes/src/edu/clayton/primeirasemana/TiposDados.java
@@ -0,0 +1,13 @@
+package edu.clayton.primeirasemana;
+
+public class TiposDados {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2023;
+        int cep = 21070333;
+        long cpf = 99933366677L;
+        float pi = 3.14F; 
+        double salario = 1275.23;
+        String nome = "Fulano de Tal";
+    }
+}
--- java-anatomia-de-classes/src/edu/clayton/primeirasemana/TiposVariaveis.java
+++ java-anatomia-de-classes/src/edu/clayton/primeirasemana/TiposVariaveis.java
@@ -1,5 +1,24 @@
 package edu.clayton.primeirasemana;
 
-public class TiposVariaveis {
-    
+public class TiposVariaveis {    
+
+    public static void main(String[] args) {
+        double salarioMinino = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        // a palavra reservada 'fimal' impede que o valor seja alerado
+        // passando assim a variavel com comportamento de constante
+        final double VALOR_DE_PI = 3.14;
+        // VALOR_DE_PI = 5.14;
+
+        System.out.println(VALOR_DE_PI);
+
+
+    }
 }
